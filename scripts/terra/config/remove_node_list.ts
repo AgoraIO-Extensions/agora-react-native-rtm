d23f7e7c3a16b5ae8a409de34a680c4326d41bea
gxz <guoxianzhe@agora.io>
2024-01-05 20:57:41 +0800

    chore: optimize

---
+++ scripts/terra/config/remove_node_list.ts
@@ -0,0 +1,1 @@
+module.exports = ['agora::rtm::IMetadata'];
--- scripts/terra/utils.ts
+++ scripts/terra/utils.ts
@@ -1,27 +1,14 @@
 import {
-  CXXFile,
   MemberFunction,
   MemberVariable,
   Struct,
 } from '@agoraio-extensions/cxx-parser';
 
-const path = require('path');
-
 let regMap: any = {
   isCallback: '.*(Observer|Handler|Callback|Receiver|Sink).*',
 };
 
-const filterFileList = require('./config/filter_file_list.json');
 const specialConstructList = require('./config/special_construct_list.json');
-
-export function filterFile(cxxfiles: CXXFile[]): CXXFile[] {
-  return cxxfiles.filter((file) => {
-    const fileName = path.basename(file.file_path);
-    return !filterFileList.some((filter: string) =>
-      new RegExp(filter, 'g').test(fileName)
-    );
-  });
-}
 
 export function isMatch(str: string, type: string): boolean {
   let result = false;
